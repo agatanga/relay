a1474dbb282ee3f77c885fcaedcdac84365d8e23
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2022-01-18 23:17:54 +0200

    Progress/finished/running methods added. Name getter added.

--- src/Models/JobBatch.php
+++ src/Models/JobBatch.php
@@ -16,6 +16,59 @@ class JobBatch extends Model
         parent::__construct($attributes);
     }
 
+    public function progress()
+    {
+        $progress = $this->total_jobs > 0 ?
+            round(($this->processedJobs() / $this->total_jobs) * 100) :
+            0;
+
+        list($min, $max) = array_values($this->range);
+
+        return round($min + ($progress * ($max - $min) / 100));
+    }
+
+    public function processedJobs()
+    {
+        return $this->total_jobs - $this->pending_jobs;
+    }
+
+    public function finished()
+    {
+        return !is_null($this->finished_at) && $this->range['max'] === 100;
+    }
+
+    public function running()
+    {
+        return !$this->failed_jobs && !$this->finished();
+    }
+
+    public function getNameAttribute($value)
+    {
+        if ($value) {
+            return substr($value, 0, strpos($value, '|['));
+        }
+
+        return $value;
+    }
+
+    public function getRangeAttribute()
+    {
+        preg_match('/\[(\d+)\/(\d+)\]$/', $this->attributes['name'] ?? '', $matches);
+
+        $min = 0;
+        $max = 100;
+
+        if ($matches) {
+            $min = (($matches[1] - 1) / $matches[2]) * 100;
+            $max = ($matches[1] / $matches[2]) * 100;
+        }
+
+        return [
+            'min' => $min,
+            'max' => $max,
+        ];
+    }
+
     public function scopeWhereMeta(Builder $query, $key, $value): Builder
     {
         if (!is_array($value)) {
