cd783a978f76859f8bf9bb5873bd5bae2fd3a3db
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2022-12-10 00:13:13 +0200

    Prevent runtime error "Call to undefined method withBatchId" This happens when job doesn't use Batchable trait.

--- src/LazyBatch.php
+++ src/LazyBatch.php
@@ -3,6 +3,7 @@
 namespace Agatanga\Relay;
 
 use Illuminate\Support\Facades\Bus;
+use Illuminate\Bus\Batchable;
 use Laravel\SerializableClosure\SerializableClosure;
 
 class LazyBatch
@@ -17,6 +18,8 @@ class LazyBatch
 
     public function __construct($name, $jobs, $method)
     {
+        $this->validateJobs($jobs);
+
         $this->name = $name;
         $this->jobs = $jobs;
         $this->method = $method;
@@ -45,5 +48,18 @@ class LazyBatch
         }
 
         return $batch->dispatch();
+    }
+
+    private function validateJobs(array $jobs)
+    {
+        foreach ($jobs as $job) {
+            if (is_array($job)) {
+                return $this->validateJobs($job);
+            }
+
+            if (!in_array(Batchable::class, class_uses_recursive($job))) {
+                throw new \RuntimeException('Class ['.get_class($job).'] does not use the [Illuminate\Bus\Batchable] trait.');
+            }
+        }
     }
 }
